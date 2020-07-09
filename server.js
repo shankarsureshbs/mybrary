4f47e893c562d69dba132233364667998908c7a2
shankarsureshbs <shankarsureshbs@gmail.com>
2020-07-09 09:32:11 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+//DB Connection with Mongoose ODM/ORM
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser : true, useUnifiedTopology : true })
+
+app.set('view engine','ejs')
+app.set('views',__dirname+"/views")
+app.set('layout','layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const db = mongoose.connection
+db.on('error',error => console.log(error))
+db.once('open',()=>console.log("Connected to Mongoose"))
+
+app.use('/',indexRouter)
+
+app.listen(process.env.PORT || 5000)
